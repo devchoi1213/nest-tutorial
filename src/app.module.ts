ac3cf6b35e712889659992404143e55e21b9d268
ChoiChanguk <devchoi1213@gmail.com>
2022-10-08 23:29:57 +0900

    @nestjs/config 패키지를 이용해 환경변수 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import {AppController} from "./app.controller";
 
+//TODO @nestjs/config 패키지를 사용하지 않고 .env 파일이 존재하는 folder를 
+// 동적으로 전달할 수 있는 커스텀 동적 모듈 구현하기
 @Module({
-  imports: [UsersModule],
-  controllers: [],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: (process.env.NODE_ENV === 'production') ? '.production.env'
+          : (process.env.NODE_ENV === 'stage') ? '.stage.env' : '.development.env'
+    })
+  ],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
