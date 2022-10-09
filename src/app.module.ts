9bb7d970aa0ca2a2de87be204658e3627f974bb9
ChoiChanguk <devchoi1213@gmail.com>
2022-10-09 11:38:05 +0900

    TypeORM 초기 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import {AppController} from "./app.controller";
 import emailConfig from "./config/emailConfig";
 import {validationSchema} from "./config/emailConfig.validation";
 import databaseConfig from "./config/databaseConfig";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 //TODO @nestjs/config 패키지를 사용하지 않고 .env 파일이 존재하는 folder를 
 // 동적으로 전달할 수 있는 커스텀 동적 모듈 구현하기
@@ -17,6 +18,16 @@ import databaseConfig from "./config/databaseConfig";
       load: [emailConfig, databaseConfig],
       isGlobal: true,
       validationSchema,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST, //'localhost',
+      port: 3306,
+      username: process.env.DATABASE_USERNAME, //'root',
+      password: process.env.DATABASE_PASSWORD, //'test',
+      database: 'test',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE), // true,
     }),
   ],
   controllers: [AppController],
