17fd54c54d8859658cdf438f2cdd08e251e0e1c5
ChoiChanguk <devchoi1213@gmail.com>
2022-10-09 12:01:56 +0900

    유저 저장 로직 트랜잭션 구현

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,7 @@ import * as uuid from 'uuid';
 import {Injectable, UnprocessableEntityException} from '@nestjs/common';
 import { EmailService } from 'src/email/email.service';
 import {InjectRepository} from "@nestjs/typeorm";
-import {Repository} from "typeorm";
+import {DataSource, Repository} from "typeorm";
 import {UserEntity} from "./entity/user.entity";
 import {ulid} from "ulid";
 
@@ -11,6 +11,7 @@ export class UsersService {
     constructor(
         private emailService: EmailService,
         @InjectRepository(UserEntity) private usersRepository: Repository<UserEntity>,  // 유저 리포지토리를 주입
+        private dataSource: DataSource
     ) { }
 
     async createUser(name: string, email: string, password: string) {
@@ -28,17 +29,34 @@ export class UsersService {
 
     private async checkUserExists(emailAddress: string) {
         const user = await this.usersRepository.findOne({ where: {email: emailAddress} });
-        return user !== undefined;
+        return user !== null;
     }
 
     private async saveUser(name: string, email: string, password: string, signupVerifyToken: string) {
-        const user = new UserEntity();
-        user.id = ulid();
-        user.name = name;
-        user.email = email;
-        user.password = password;
-        user.signupVerifyToken = signupVerifyToken;
-        await this.usersRepository.save(user);
+        const queryRunner = this.dataSource.createQueryRunner();
+
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+
+        try {
+            const user = new UserEntity();
+            user.id = ulid();
+            user.name = name;
+            user.email = email;
+            user.password = password;
+            user.signupVerifyToken = signupVerifyToken;
+            await queryRunner.manager.save(user);
+
+            // throw new InternalServerErrorException(); // 일부러 에러를 발생시켜 본다
+
+            await queryRunner.commitTransaction();
+        } catch (e) {
+            // 에러가 발생하면 롤백
+            await queryRunner.rollbackTransaction();
+        } finally {
+            // 직접 생성한 QueryRunner는 해제시켜 주어야 함
+            await queryRunner.release();
+        }
     }
 
     private async sendMemberJoinEmail(email: string, signupVerifyToken: string) {
